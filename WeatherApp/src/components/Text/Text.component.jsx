950025d78312847daf9b82209143c4ccfb77eb69
Sina Farhadi <ahmadibishop@gmail.com>
2022-03-18 22:42:46 +0330

    Initilized some essentiall components

---
+++ WeatherApp/src/components/Text/Text.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const TextComponent = () => (
+    <Text>
+        {text}
+    </Text>
+);
+
---
+++ WeatherApp/src/containers/Home/Home.container.jsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import TextInputComponent from "../../components/TextInput/TextInput.component";
+
+const HomeContainer = () => {
+    return (
+        <>
+            <TextInputComponent />
+        </>
+    );
+};
+
+export default HomeContainer;
