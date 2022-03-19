e79052128d6a86cb7d47c0b360b30a83d79c8ca7
Sina Farhadi <ahmadibishop@gmail.com>
2022-03-19 16:26:32 +0330

    created needed apis

---
+++ WeatherApp/src/api/index.js
@@ -0,0 +1,26 @@
+// Api key
+import axios from "axios";
+
+const GEO_URL = "http://api.openweathermap.org/geo/1.0/direct";
+const DATA_URL = "https://api.openweathermap.org/data/2.5/onecall"
+const token = "aef6920af18107facf4fa4214b1114cb";
+
+export const get_coordinates = async (city) => {
+    // get the city lat & lng by its name
+    try {
+        const response = await axios.get(`${GEO_URL}?q=${city}&appid=${token}`);
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    };
+};
+
+export const get_weathers = async (lat, lon) => {
+    // get the lat & lng current weather condition
+    try {
+        const response = await axios.get(`${DATA_URL}?lat=${lat}&lon=${lon}&appid=${token}`);
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    };
+};
